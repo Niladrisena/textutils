5a38bb24f278fbbd9a158150daad31a8e8242783
Niladri <117145249+Niladrisena@users.noreply.github.com>
2023-11-26 10:41:47 +0530

    Alert adding

--- src/App.js
+++ src/App.js
@@ -3,19 +3,33 @@ import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 
 
 function App() {
   const [mode, setMode] = useState('light');// whether dark mode is enabled or not 
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (massage, type)=>{
+      setAlert({
+        msg: massage,
+        type:type
+      })
+      setTimeout(() => {
+        setAlert(null);
+      }, 1500);
+  }
 
   const toggleMode = ()=>{
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#20242e';
+      showAlert("Dark mode has been enabled","success");
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled","success");
     }
   }
   return (
@@ -23,8 +37,9 @@ function App() {
 {/* <Navbar title= "TextUtiles" aboutText="About TextUtiles"/> */}
 {/* <Navbar /> */}
 <Navbar title= "TextUtiles" mode={mode} toggleMode={toggleMode} />
+<Alert alert={alert}/>
 <div className="container my -2">
-<TextForm heading="Enter the text to analyze below " mode={mode}/>
+<TextForm showAlert= {showAlert}heading="Enter the text to analyze below " mode={mode}/>
 {/* <About/> */}
 </div>
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,11 +6,13 @@ export default function TextForm(props) {
         //console.log("Uppercase was Clicked" + text);
         let newText = text.toUpperCase();
         setText(newText)
+        props.showAlert("Converted to uppercase", "success")
     }
     const handleLoClick = ()=>{
         //console.log("Uppercase was Clicked" + text);
         let newText = text.toLowerCase();
         setText(newText)
+        props.showAlert("Converted to lowercase", "success")
     }
     const handleOnChange = (event)=>{
         //console.log("On Change");
@@ -21,6 +23,7 @@ export default function TextForm(props) {
       var text = document.getElementById("myBox");
       text.select();
       navigator.clipboard.writeText(text.value);
+      props.showAlert("Text copied", "success")
     }
   const [text, setText] = useState('');   
   return (
